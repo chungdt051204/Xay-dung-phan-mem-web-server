9a039f49c92709b2e6f211fff2b3fa93e17e3232
chungdt051204 <email_github_ca_bn@gmail.com>
2026-02-10 15:22:04 +0700

    Update 10/2

--- service/sendEmail.js
+++ service/sendEmail.js
@@ -1,31 +1,27 @@
 const nodemailer = require("nodemailer");
 
-const sendEmail = async (to, code, subject) => {
-  const transporter = nodemailer.createTransport({
-    host: "smtp-relay.brevo.com",
-    port: 587,
-    secure: false,
-    auth: {
-      user: "apikey",
-      pass: process.env.SMTP_KEY,
-    },
-    tls: {
-      rejectUnauthorized: false,
-    },
-  });
+const transporter = nodemailer.createTransport({
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: "apikey",
+    pass: process.env.SMTP_KEY,
+  },
+  connectionTimeout: 10000,
+});
 
-  try {
-    const info = await transporter.sendMail({
+const sendEmail = (to, code, subject) => {
+  transporter
+    .sendMail({
       from: "ChungDo <huy91856@gmail.com>",
-      to: to,
-      subject: subject,
-      text: "Confirm Code: " + code,
+      to,
+      subject,
+      text: `Confirm Code: ${code}`,
       html: `<b>Code: ${code}</b>`,
-    });
-    console.log("Message sent:", info.messageId);
-    return info;
-  } catch (err) {
-    console.log("Error occurred. " + err.message);
-  }
+    })
+    .then(() => console.log("✅ Email sent"))
+    .catch((err) => console.error("❌ Send email error:", err.message));
 };
+
 module.exports = sendEmail;
